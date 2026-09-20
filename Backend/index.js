cf9c757fe8e8b169c3cfe8572489c1449cbe1033
Aanchal <anchalkushwaha002@gmail.com>
2026-09-20 19:46:09 +0530

    Allow dashboard CORS

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,8 @@ app.use(cors({
   origin: [
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://zerodha-frontend-gzrt.onrender.com"
+    "https://zerodha-frontend-gzrt.onrender.com",
+    "https://zerodha-dashboard-86nk.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
